960d8bfdcb07e41da4d9163add319b75c2d8f543
PoorGenius <albinhasanaj06@hotmail.com>
2024-07-13 21:23:41 +0200

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpackDevMiddleware: (config) => {
+        // Solve the problem of the browser not refreshing when the code changes
+        config.watchOptions = {
+            poll: 1000,
+            aggregateTimeout: 300,
+        };
+        return config;
+    }
+};
 
 export default nextConfig;
